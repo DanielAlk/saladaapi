3969b0a8fbcba00f370e98c8b95b2e20248b4a9d
DanielAlk <daniodfad@yahoo.com.ar>
2016-09-07 05:05:38 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+SaladaApi::Application.config.secret_token = '5b247855a413c013932c2dd058e62891a179b50e36c5f4506939776c2098500e4d8ec48955285c105715f092221176007a1bf59ae53b4c817c29facbe40ce9ad'
