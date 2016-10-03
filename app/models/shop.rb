a75694ad05908424e5d4cbd088a35e370d577e4d
DanielAlk <daniodfad@yahoo.com.ar>
2016-10-03 07:20:02 -0300

    append to previous commit

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ActiveRecord::Base
 	include Filterable
-	has_attached_file :image, styles: { medium: "640x300#", small: "297x257#", thumb: "127x127#" }, default_url: lambda { |a| "#{a.instance.create_default_url}" }
+	has_attached_file :image, styles: { medium: "640x300#", small: "297x257#", thumb: "127x127#" }, default_url: lambda { |a| "#{a.instance.image_default_url}" }
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
   belongs_to :user
   belongs_to :shed
@@ -35,7 +35,7 @@ class Shop < ActiveRecord::Base
     self.shed.title
   end
 
-  def create_default_url
+  def image_default_url
     ActionController::Base.helpers.asset_url("missing.png", :digest => false)
   end
 
