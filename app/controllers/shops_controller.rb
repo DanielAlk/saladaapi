b3334b980c005079096248f26a821bf3764541ed
DanielAlk <daniodfad@yahoo.com.ar>
2020-07-15 00:05:05 -0300

    [Shops] Create and claim

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -47,6 +47,24 @@ class ShopsController < ApplicationController
     end
   end
 
+  # POST /shops/create_and_claim
+  # POST /shops/create_and_claim.json
+  def create_and_claim
+    @shop = Shop.new(shop_params)
+    @shop.user = User.admin.first
+
+    if @shop.save
+      shop_claim = ShopClaim.new(shop: @shop, user: current_user)
+      if shop_claim.save
+        render json: @shop, status: :created, location: @shop
+      else
+        render json: shop_claim.errors, status: :unprocessable_entity
+      end
+    else
+      render json: @shop.errors, status: :unprocessable_entity
+    end
+  end
+
   # PATCH/PUT /shops/1
   # PATCH/PUT /shops/1.json
   def update
