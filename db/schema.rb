184c9b89b23de6156f02d937627106ff4c817f2c
DanielAlk <daniodfad@yahoo.com.ar>
2020-10-29 17:00:53 -0300

    [Models] Query and History (managed by php logic for Facu)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200926070956) do
+ActiveRecord::Schema.define(version: 20201029195916) do
 
   create_table "admin_notifications", force: :cascade do |t|
     t.integer  "kind",           limit: 4,     default: 0
@@ -91,6 +91,16 @@ ActiveRecord::Schema.define(version: 20200926070956) do
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
   end
+
+  create_table "histories", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "product_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "histories", ["product_id"], name: "index_histories_on_product_id", using: :btree
+  add_index "histories", ["user_id"], name: "index_histories_on_user_id", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.string   "title",             limit: 255
@@ -255,6 +265,13 @@ ActiveRecord::Schema.define(version: 20200926070956) do
     t.datetime "updated_at",                                                      null: false
   end
 
+  create_table "queries", force: :cascade do |t|
+    t.text     "search",     limit: 65535
+    t.integer  "count",      limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "product_id", limit: 4
@@ -395,6 +412,8 @@ ActiveRecord::Schema.define(version: 20200926070956) do
   add_foreign_key "comments", "interactions"
   add_foreign_key "comments", "users"
   add_foreign_key "comments", "users", column: "receiver_id"
+  add_foreign_key "histories", "products"
+  add_foreign_key "histories", "users"
   add_foreign_key "interactions", "comments", column: "last_comment_id"
   add_foreign_key "interactions", "products"
   add_foreign_key "interactions", "users"
